791fc7d9073a420bf42be11c758b33d1bbd598d3
rsabro <rsabro@gmail.com>
2026-08-04 12:21:34 -0300

    Ajusta logo

--- js/script.js
+++ js/script.js
@@ -1,35 +1,20 @@
 // trocar tema dia/noite
-
 const tema = document.querySelector("#tema");
-
 tema.onclick = function(){
-
     document.body.classList.toggle("dark");
-
     if(document.body.classList.contains("dark")){
         tema.innerHTML="☀️";
     }
     else{
         tema.innerHTML="🌙";
     }
-
 };
 
-
 // leitura por voz
-
 const voz = document.querySelector("#voz");
-
-
 voz.onclick=function(){
-
-let texto=document.body.innerText;
-
-
-let leitura = new SpeechSynthesisUtterance(texto);
-
-leitura.lang="pt-BR";
-
-speechSynthesis.speak(leitura);
-
+    let texto=document.body.innerText;
+    let leitura = new SpeechSynthesisUtterance(texto);
+    leitura.lang="pt-BR";
+    speechSynthesis.speak(leitura);
 };
